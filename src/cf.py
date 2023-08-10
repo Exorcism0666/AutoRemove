66dd0f1378e8212c65b5cac6d530aa1b41dad11f
CoolPlayLin <CoolPlayLin@qq.com>
2023-08-10 18:35:45 +0800

    up

--- src/cf.py
+++ src/cf.py
@@ -14,6 +14,6 @@ for each in range(1, 8):
             URLs = [i["browser_download_url"] for i in each["assets"] if "compiler" in i["browser_download_url"] and i["browser_download_url"][-4::] == ".zip"]
             URLs = str(URLs).replace("[", "").replace("]", "").replace("'", "").replace(" ", "")
             print(f"java -jar Komac-1.9.1-all.jar up --id JetBrains.Kotlin.Compiler --version {Version} --urls {URLs} --submit")
-            system(f"java -jar Komac-1.9.1-all.jar --id JetBrains.Kotlin.Compiler --version {Version} --urls {URLs} --submit --token {Token}")
+            system(f"java -jar Komac-1.9.1-all.jar up --id JetBrains.Kotlin.Compiler --version {Version} --urls {URLs} --submit --token {Token}")
         except BaseException as e:
             print(e)
