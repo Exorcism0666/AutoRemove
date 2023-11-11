b05b49af113b9fc0c1032a82bb641c59a00fa98e
CoolPlayLin <CoolPlayLin@qq.com>
2023-11-11 17:04:55 +0800

    feat: include all issue

--- src/comment.ts
+++ src/comment.ts
@@ -30,59 +30,65 @@ const login = "coolplaylinbot";
     )}`,
   );
   let content = readFileSync("./docs/template.md", "utf-8");
-  const data = (
-    await api.rest.search.issuesAndPullRequests({
-      q: "user:microsoft repo:winget-pkgs author:coolplaylinbot state:open is:pr",
-      per_page: 100,
-    })
-  ).data.items;
-  if (data.length >= 1) {
-    console.log(
-      `${colors.green(
-        `We've found ${data.length} pull request(s) that opened by coolplaylinbot`,
-      )}`,
-    );
-  } else {
-    console.log(
-      `${colors.green(
-        `We haven't found pull request that opened by coolplaylinbot`,
-      )}`,
-    );
-  }
-  data.forEach(async (obj) => {
-    const comments = await api.rest.issues.listComments({
-      repo: repo,
-      owner: owner,
-      issue_number: obj.number,
-    });
-    const ready = comments.data.filter((obj) => {
-      if (obj.user?.login == login && obj.body?.includes("## For moderators")) {
-        return obj;
-      }
-    });
-    console.log(
-      `${colors.underline(
-        colors.green(`[${data.indexOf(obj) + 1}/${data.length}]`),
-      )} ${colors.blue("issue")} ${colors.yellow(`#${obj.number}`)}`,
-    );
-    if (ready.length === 0) {
-      const res = await api.issues.createComment({
-        repo: repo,
-        owner: owner,
-        issue_number: obj.number,
-        body: content,
-      });
+  let i = 0;
+  while (true) {
+    i += 1
+    const data = (
+      await api.rest.search.issuesAndPullRequests({
+        q: "user:microsoft repo:winget-pkgs author:coolplaylinbot state:open is:pr",
+        per_page: 100,
+        page: i
+      })
+    ).data.items;
+    if (data.length >= 1) {
       console.log(
-        `${colors.green(`Comment in issue`)} ${colors.yellow(
-          `#${obj.number}`,
-        )} ${colors.green(`posts succeed`)}`,
+        `${colors.green(
+          `We've found ${data.length} pull request(s) that opened by coolplaylinbot`,
+        )}`,
       );
     } else {
       console.log(
-        `${colors.blue(`Comment in issue`)} ${colors.yellow(
-          `#${obj.number}`,
-        )} ${colors.blue("has been already posted.")}`,
+        `${colors.green(
+          `We haven't found pull request that opened by coolplaylinbot`,
+        )}`,
       );
+      break
     }
-  });
+    data.forEach(async (obj) => {
+      const comments = await api.rest.issues.listComments({
+        repo: repo,
+        owner: owner,
+        issue_number: obj.number,
+      });
+      const ready = comments.data.filter((obj) => {
+        if (obj.user?.login == login && obj.body?.includes("## For moderators")) {
+          return obj;
+        }
+      });
+      console.log(
+        `${colors.underline(
+          colors.green(`[${data.indexOf(obj) + 1}/${data.length}]`),
+        )} ${colors.blue("issue")} ${colors.yellow(`#${obj.number}`)}`,
+      );
+      if (ready.length === 0) {
+        const res = await api.issues.createComment({
+          repo: repo,
+          owner: owner,
+          issue_number: obj.number,
+          body: content,
+        });
+        console.log(
+          `${colors.green(`Comment in issue`)} ${colors.yellow(
+            `#${obj.number}`,
+          )} ${colors.green(`posts succeed`)}`,
+        );
+      } else {
+        console.log(
+          `${colors.blue(`Comment in issue`)} ${colors.yellow(
+            `#${obj.number}`,
+          )} ${colors.blue("has been already posted.")}`,
+        );
+      }
+    });
+  }
 })();
