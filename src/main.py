a2280235bfaa3ce7a188fba305e1f0a777b38774
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-01 13:08:41 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -310,19 +310,6 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
-# Add Audacity.Audacity to Update List
-    id = "Audacity.Audacity"
-    JSON = requests.get("https://api.github.com/repos/audacity/audacity/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
-    Version = requests.get("https://api.github.com/repos/audacity/audacity/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("blockmap" in each["browser_download_url"]) and (("64bit" in each["browser_download_url"]) or ("32bit" in each["browser_download_url"]))]
-    if not version_verify(Version, id):
-        report_existed(id, Version)
-    elif do_list(id, Version, "verify"):
-        report_existed(id, Version)
-    else:
-        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
-    del JSON, Urls, Version, id
-
 # Add SchildiChat.SchildiChat to Update List
     id = "SchildiChat.SchildiChat"
     JSON = requests.get("https://api.github.com/repos/SchildiChat/schildichat-desktop/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
