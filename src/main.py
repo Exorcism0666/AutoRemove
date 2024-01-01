5cd1e96ee4f7c24374726cca49625621de9a7f7c
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-01 16:10:58 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -179,6 +179,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     else:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
+
+# Add yt-dlp.yt-dlp to Update List
+    id = "yt-dlp.yt-dlp"
+    JSON = requests.get("https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe") and not each["browser_download_url"].endswith("min.exe")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
     
     # Updating
     if not debug:
