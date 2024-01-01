2e4981e990b491fc9b29966b65c3686ace51f9c2
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-01 23:13:30 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -366,7 +366,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "AivarAnnamaa.Thonny"
     JSON = requests.get("https://api.github.com/repos/thonny/thonny/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/thonny/thonny/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not(("xxl" in each["browser_download_url"]) or ("py38" in each["browser_download_url"]))]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
