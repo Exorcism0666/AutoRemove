bece89e3bed71644155917eff89309d8a1196248
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-01 14:41:39 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -140,6 +140,45 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     else:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
+
+# Add Tyrrrz.DiscordChatExporter.CLI to Update List
+    id = "Tyrrrz.DiscordChatExporter.CLI"
+    JSON = requests.get("https://api.github.com/repos/Tyrrrz/DiscordChatExporter/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/Tyrrrz/DiscordChatExporter/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".Cli.zip")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+# Add Tyrrrz.DiscordChatExporter.GUI to Update List
+    id = "Tyrrrz.DiscordChatExporter.GUI"
+    JSON = requests.get("https://api.github.com/repos/Tyrrrz/DiscordChatExporter/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/Tyrrrz/DiscordChatExporter/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith("ChatExporter.zip")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+# Add Tyrrrz.LightBulb to Update List
+    id = "Tyrrrz.LightBulb"
+    JSON = requests.get("https://api.github.com/repos/Tyrrrz/LightBulb/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/Tyrrrz/LightBulb/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
     
     # Updating
     if not debug:
