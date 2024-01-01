735e0b6c53dadb9442711efee451c029448ff2be
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-01 23:02:42 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -388,6 +388,32 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+    # Add AppbyTroye.KoodoReader to Update List
+    id = "AppbyTroye.KoodoReader"
+    JSON = requests.get("https://api.github.com/repos/troyeguo/koodo-reader/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/troyeguo/koodo-reader/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not(("blockmap" in each["browser_download_url"]) or ("Portable" in each["browser_download_url"]))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+    # Add clouDr-f2e.rubick to Update List
+    id = "clouDr-f2e.rubick"
+    JSON = requests.get("https://api.github.com/repos/rubickCenter/rubick/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/rubickCenter/rubick/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("blockmap" in each["browser_download_url"]) and (("x64" in each["browser_download_url"]) or ("ia32" in each["browser_download_url"]))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
