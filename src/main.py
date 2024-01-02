03aaed4b039d99f54fb87d09049008fe1bd7843f
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-02 14:17:20 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -596,6 +596,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+    # Add Ombrelin.PlexRichPresence to Update List
+    id = "Ombrelin.PlexRichPresence"
+    JSON = requests.get("https://api.github.com/repos/Ombrelin/plex-rich-presence/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/Ombrelin/plex-rich-presence/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if "exe " in each["browser_download_url"]]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
