579897c2bab3f2b563c7886c9b15605a757737da
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-02 23:05:57 +0100

    Update and fix

--- src/main.py
+++ src/main.py
@@ -638,8 +638,21 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     # Add Retroshare.Retroshare to Update List
     id = "Retroshare.Retroshare"
     JSON = requests.get("https://api.github.com/repos/RetroShare/RetroShare/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
-    Version = requests.get("https://api.github.com/repos/RetroShare/RetroSharereleases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Version = requests.get("https://api.github.com/repos/RetroShare/RetroShare/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if "exe" in each["browser_download_url"]]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+    # Add Fly-io.flyctl to Update List
+    id = "Fly-io.flyctl"
+    JSON = requests.get("https://api.github.com/superfly/flyctl/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/superfly/flyctl/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if "zip" in each["browser_download_url"]]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
