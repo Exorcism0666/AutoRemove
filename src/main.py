4685b75dc0040c912453d81e843b0bd0f9a14e4b
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-02 23:58:54 +0100

    my god kms

--- src/main.py
+++ src/main.py
@@ -651,7 +651,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     # Add Fly-io.flyctl to Update List
     id = "Fly-io.flyctl"
     JSON = requests.get("https://api.github.com/repos/superfly/flyctl/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
-    Version = requests.get("https://api.github.com/superfly/flyctl/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Version = requests.get("https://api.github.com/repos/superfly/flyctl/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if "zip" in each["browser_download_url"]]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
