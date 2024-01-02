12081cbbcbd084ceb536e9be57460c76fc5cb9eb
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-02 20:53:02 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -483,7 +483,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "staniel359.muffon"
     JSON = requests.get("https://api.github.com/repos/staniel359/muffon/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/staniel359/muffon/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and (("portable" in each["browser_download_url"]) or ("x64" in each["browser_download_url"]))]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
@@ -600,6 +600,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "Ombrelin.PlexRichPresence"
     JSON = requests.get("https://api.github.com/repos/Ombrelin/plex-rich-presence/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/Ombrelin/plex-rich-presence/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if "exe" in each["browser_download_url"]]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+    # Add PurpleI2P.i2pd to Update List
+    id = "PurpleI2P.i2pd"
+    JSON = requests.get("https://api.github.com/repos/PurpleI2P/i2pd/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/PurpleI2P/i2pd/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if "exe" in each["browser_download_url"]]
     if not version_verify(Version, id):
         report_existed(id, Version)
