139d11ee51ddf21760da9ecbe32556737d333f1e
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-03 13:10:32 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -769,7 +769,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "GeekCorner.threema"
     JSON = requests.get("https://api.github.com/repos/GeekCornerGH/threema-for-desktop/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/GeekCornerGH/threema-for-desktop/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if ".exe" in each["browser_download_url"]]
+    Urls = [each["browser_download_url"] for each in JSON if "Threema-For-Desktop-setup" in each["browser_download_url"]]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
