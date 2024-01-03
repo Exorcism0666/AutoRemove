c47d9027ae70ab5163e5df42e7aeb923dccdb0a6
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 00:24:55 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -804,6 +804,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Telegram.TelegramDesktop to Update List
+    id = "Telegram.TelegramDesktop"
+    JSON = requests.get("https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith((".zip", ".exe"))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
