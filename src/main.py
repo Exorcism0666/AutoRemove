20553ff3b26e24068b414e9b1091fef363e93cb4
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 10:48:46 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -843,6 +843,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add xavidop.cxcli to Update List
+    id = "xavidop.cxcli"
+    JSON = requests.get("https://api.github.com/repos/xavidop/dialogflow-cx-cli/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/xavidop/dialogflow-cx-cli/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not(("sbom" in each["browser_download_url"]) or ("armv7" in each["browser_download_url"])) and (("arm64" in each["browser_download_url"]) or ("x86_64" in each["browser_download_url"]) or ("i386" in each["browser_download_url"]))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
