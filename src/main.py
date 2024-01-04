f2f351cc53c3b78d391eb6092f5fe246c955e502
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 18:51:17 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -961,6 +961,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add hikari-no-yume.touchHLE to Update List
+    id = "hikari-no-yume.touchHLE"
+    JSON = requests.get("https://api.github.com/repos/hikari-no-yume/touchHLE/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/hikari-no-yume/touchHLE/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith("Windows_x86_64.zip")]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
