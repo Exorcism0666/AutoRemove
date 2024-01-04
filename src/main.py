b48336e0d063a0cc56f2edc058ade9c7fefc0652
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 11:49:30 +0100

    Re add openutau

--- src/main.py
+++ src/main.py
@@ -895,6 +895,20 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add stakira.OpenUTAU to Update List
+    id = "stakira.OpenUTAU"
+    JSON = requests.get("https://api.github.com/repos/stakira/OpenUtau/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/stakira/OpenUtau/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Version = Version.replace("bluid/", "")
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
