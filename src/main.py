5199c0ce84f08d4853f8ddc4267cd841beaf7f13
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 18:56:46 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -974,6 +974,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add LIJI32.SameBoy to Update List
+    id = "LIJI32.SameBoy"
+    JSON = requests.get("https://api.github.com/repos/LIJI32/SameBoy/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/LIJI32/SameBoy/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not("cocoa" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
