a3998f8d15e4c2f5d6dfe4c25ebe85d3fb0504a8
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 11:35:36 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -887,12 +887,12 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     JSON = requests.get("https://api.github.com/repos/chawyehsu/hok/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/chawyehsu/hok/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not("sha256" in each["browser_download_url"]) and (("aarch64" in each["browser_download_url"]) or ("i686" in each["browser_download_url"]) or ("x86_64" in each["browser_download_url"]))]
-    if not version_verify(Version, id):
-        report_existed(id, Version)
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
     elif do_list(id, Version, "verify"):
         report_existed(id, Version)
     else:
-        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
     # Updating
