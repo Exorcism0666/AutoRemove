aa2d8830223313cf564671b2d5a5295fab371395
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-01-04 11:24:57 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -856,6 +856,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Chocolatey.ChocolateyGUI to Update List
+    id = "Chocolatey.ChocolateyGUI"
+    JSON = requests.get("https://api.github.com/repos/chocolatey/ChocolateyGUI/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/chocolatey/ChocolateyGUI/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".msi")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
