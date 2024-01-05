2201cc5b33e480df82d6ab1d3ec44ac6f593c943
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-05 19:40:35 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1129,6 +1129,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Bruno.Bruno to Update List
+    id = "Bruno.Bruno"
+    JSON = requests.get("https://api.github.com/repos/usebruno/bruno/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/usebruno/bruno/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
