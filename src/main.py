c3a22db68a9f6ad742001e193ccd3957470927ea
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-05 11:06:25 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -999,7 +999,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
-   # Add Joplin.Joplinto Update List
+   # Add Joplin.Joplin to Update List
     id = "Joplin.Joplin"
     JSON = requests.get("https://api.github.com/repos/laurent22/joplin/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/laurent22/joplin/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
@@ -1010,6 +1010,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         report_existed(id, Version)
     else:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+   # Add Status.Status to Update List
+    id = "Status.Status"
+    JSON = requests.get("https://api.github.com/repos/status-im/status-desktop/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/status-im/status-desktop/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
     # Updating
