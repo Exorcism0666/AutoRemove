ede9b491192b72ff7b416f681dcb4d5d4d7b2812
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-05 21:47:58 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1181,6 +1181,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Notepad++.Notepad++ to Update List
+    id = "Notepad++.Notepad++"
+    JSON = requests.get("https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("sig" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
