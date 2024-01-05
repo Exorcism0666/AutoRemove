f5fdbbb0680ea4e6dd96309853c8940a1035cd4c
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-05 17:19:28 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1064,6 +1064,32 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Hugo.Hugo to Update List
+    id = "Hugo.Hugo"
+    JSON = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not("extended" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+   # Add Hugo.Hugo.Extended to Update List
+    id = "Hugo.Hugo.Extended"
+    JSON = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and ("extended" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
