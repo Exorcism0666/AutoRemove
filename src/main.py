18dbe64bfff7d876a6cce2f53d39b324c936c0f7
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-05 20:46:54 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1155,6 +1155,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add IPEP.Scantailor-Experimental to Update List
+    id = "IPEP.Scantailor-Experimental"
+    JSON = requests.get("https://api.github.com/repos/ImageProcessing-ElectronicPublications/scantailor-experimental/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/ImageProcessing-ElectronicPublications/scantailor-experimental/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("X86-64-Qt6" in each["browser_download_url"])]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
