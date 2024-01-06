4738aa56ef4cc72d5291e053fa8a24f5876a2b27
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-01-06 12:28:09 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1207,6 +1207,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add SagerNet.sing-box to Update List
+    id = "SagerNet.sing-box"
+    JSON = requests.get("https://api.github.com/repos/SagerNet/sing-box/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/SagerNet/sing-box/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not(("amd64v3" in each["browser_download_url"]) or ("universal" in each["browser_download_url"]) or ("legacy" in each["browser_download_url"])) and (("arm64" in each["browser_download_url"]) or ("amd64" in each["browser_download_url"]) or ("386" in each["browser_download_url"]))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
