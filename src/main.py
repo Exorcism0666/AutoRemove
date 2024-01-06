9eb31ee36ecaa5e4a5d166490575d14f9f62011d
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-06 22:26:07 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1246,18 +1246,18 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
-    # # Add AGSProjectTeam.AdventureGameStudio to Update List
+    # Add AGSProjectTeam.AdventureGameStudio to Update List
     # id = "AGSProjectTeam.AdventureGameStudio"
-    # JSON = requests.get("https://api.github.com/repos/adventuregamestudio/ags/latest", verify=False, headers=Headers[1]).json()["assets"]
-    # Version = requests.get("https://api.github.com/repos/adventuregamestudio/ags/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    # Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
-    # if not version_verify(str_pop(Version, 0), id):
-    #     report_existed(id, Version)
-    # elif do_list(id, Version, "verify"):
-    #     report_existed(id, Version)
-    # else:
-    #    Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
-    # del JSON, Urls, Version, id
+    JSON = requests.get("https://api.github.com/repos/adventuregamestudio/ags/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/adventuregamestudio/ags/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(str_pop(Version, 0), id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+       Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
 
    # Add Rinrab.AOVpnManager to Update List
     id = "Rinrab.AOVpnManager"
