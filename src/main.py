2f1b908b447afc27823c0168189643e3f5410681
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-07 02:48:56 +0000

    Update

--- src/main.py
+++ src/main.py
@@ -1272,6 +1272,32 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add TailwindLabs.TailwindCSS to Update List
+    id = "TailwindLabs.TailwindCSS"
+    JSON = requests.get("https://api.github.com/repos/tailwindlabs/tailwindcss/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/tailwindlabs/tailwindcss/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+   # Add Alex313031.Thorium.AVX2 to Update List
+    id = "Alex313031.Thorium.AVX2"
+    JSON = requests.get("https://api.github.com/repos/Alex313031/Thorium-Win-AVX2/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/Alex313031/Thorium-Win-AVX2/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
