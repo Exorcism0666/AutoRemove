3b4635482809f8f3f81b28535518277e263a3b0c
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-08 20:45:15 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1003,7 +1003,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "Joplin.Joplin"
     JSON = requests.get("https://api.github.com/repos/laurent22/joplin/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/laurent22/joplin/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("Portable" in each["browser_download_url"])]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
