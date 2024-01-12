c7e07334d9ada77bdccc6875bd871195fa731247
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-12 18:12:15 +0100

    Update lmao

--- src/main.py
+++ src/main.py
@@ -1624,17 +1624,17 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     del JSON, Urls, Version, id
 
    # Add Jubler.App to Update List
-        id = "Jubler.App"
-        JSON = requests.get("https://api.github.com/repos/teras/Jubler/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
-        Version = requests.get("https://api.github.com/repos/teras/Jubler/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-        Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
-        if not version_verify(str_pop(Version, 0), id):
-            report_existed(id, Version)
-        elif do_list(id, Version, "verify"):
-            report_existed(id, Version)
-        else:
-            Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
-        del JSON, Urls, Version, id
+    id = "Jubler.App"
+    JSON = requests.get("https://api.github.com/repos/teras/Jubler/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/teras/Jubler/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(str_pop(Version, 0), id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
 
    # Add Task.Task to Update List
     id = "Task.Task"
