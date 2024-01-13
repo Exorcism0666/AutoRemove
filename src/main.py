1436b9d8ca467383817cf7a588c7a86b3ba56f6c
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-13 14:46:30 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1701,6 +1701,32 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add SagerNet.sing-box to Update List
+    id = "SagerNet.sing-box"
+    JSON = requests.get("https://api.github.com/repos/SagerNet/sing-box/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/SagerNet/sing-box/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not(("universal" in each["browser_download_url"]) or ("legacy" in each["browser_download_url"]) or ("v3" in each["browser_download_url"]))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+   # Add lycheeverse.lychee to Update List
+    id = "lycheeverse.lychee"
+    JSON = requests.get("https://api.github.com/repos/lycheeverse/lychee/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/lycheeverse/lychee/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
