5ef46a57952cb14dab4f397ac5289eead96964a7
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-13 13:11:59 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -882,6 +882,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Ryujinx.Ryujinx to Update List
+    id = "Ryujinx.Ryujinx"
+    JSON = requests.get("https://api.github.com/repos/Ryujinx/release-channel-master/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/Ryujinx/release-channel-master/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not(("sdl2" in each["browser_download_url"]) or ("ava" in each["browser_download_url"]))]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
    # Add Ryujinx.Ryujinx.Ava to Update List
     id = "Ryujinx.Ryujinx.Ava"
     JSON = requests.get("https://api.github.com/repos/Ryujinx/release-channel-master/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
