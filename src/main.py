3731987a3e64deb26936137b5ba756534e4b9a51
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-13 21:48:38 +0100

    Remove

--- src/main.py
+++ src/main.py
@@ -1207,19 +1207,6 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
-   # Add LutzRoeder.Netron to Update List
-    id = "LutzRoeder.Netron"
-    JSON = requests.get("https://api.github.com/repos/lutzroeder/netron/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
-    Version = requests.get("https://api.github.com/repos/lutzroeder/netron/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
-    if not version_verify(str_pop(Version, 0), id):
-         report_existed(id, Version)
-    elif do_list(id, Version, "verify"):
-        report_existed(id, Version)
-    else:
-        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
-    del JSON, Urls, Version, id
-
    # Add SagerNet.sing-box to Update List
     id = "SagerNet.sing-box"
     JSON = requests.get("https://api.github.com/repos/SagerNet/sing-box/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
