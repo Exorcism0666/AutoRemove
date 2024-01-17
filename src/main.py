b4068a1fcf117be0f1e000275e1748214ee53ccd
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-17 21:06:29 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1987,6 +1987,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Oxen.Session to Update List
+    id = "Oxen.Session"
+    JSON = requests.get("https://api.github.com/repos/oxen-io/session-desktop/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/oxen-io/session-desktop/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("blockmap" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
