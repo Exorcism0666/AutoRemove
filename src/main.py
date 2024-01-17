3937cabb6b2dcc5a6d8de443ae8da7bcdd6fd9df
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-17 07:58:56 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -1809,7 +1809,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "Ablaze.Floorp"
     JSON = requests.get("https://api.github.com/repos/Floorp-Projects/Floorp/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/Floorp-Projects/Floorp/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and not("stub" in each["browser_download_url"])]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("stub" in each["browser_download_url"])]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
