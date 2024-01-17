44619a79afa9fd529a461bcb5567f6eb18f06957
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-17 15:49:04 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1857,6 +1857,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Streetwriters.Notesnook to Update List
+    id = "Streetwriters.Notesnook"
+    JSON = requests.get("https://api.github.com/repos/streetwriters/notesnook/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/streetwriters/notesnook/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("blockmap" in each["browser_download_url"]) and (("arm64" in each["browser_download_url"]) or ("x64" in each["browser_download_url"]))]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
