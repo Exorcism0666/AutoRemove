238d43bf7eed351d42f3525c3148694822c8c089
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-23 06:12:57 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2199,7 +2199,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
    # Add Bitwarden.Bitwarden to Update List
     id = "Bitwarden.Bitwarden"
     JSON = requests.get("https://api.github.com/repos/bitwarden/clients/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
-    Version = re.sub(r'[^\d.]', '', requests.get("https://api.github.com/repos/bitwarden/clients/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]) if tag_name.startswith("desktop-") else None
+    Version = re.sub(r'[^\d.]', '', requests.get("https://api.github.com/repos/bitwarden/clients/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"])
     Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
