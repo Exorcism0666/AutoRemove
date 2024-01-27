a13a99b92082c03a26556964ef6807d09d85556e
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-27 20:32:22 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -2279,12 +2279,12 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     JSON = requests.get("https://api.github.com/repos/Gnucash/gnucash/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/Gnucash/gnucash/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
-    if not version_verify(str_pop(Version, 0), id):
-         report_existed(id, Version)
+    if not version_verify(Version, id):
+        report_existed(id, Version)
     elif do_list(id, Version, "verify"):
         report_existed(id, Version)
     else:
-        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
     # Add JohnMacFarlane.Pandoc to Update List
@@ -2292,12 +2292,12 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     JSON = requests.get("https://api.github.com/repos/jgm/pandoc/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/jgm/pandoc/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".msi")]
-    if not version_verify(str_pop(Version, 0), id):
-         report_existed(id, Version)
+    if not version_verify(Version, id):
+        report_existed(id, Version)
     elif do_list(id, Version, "verify"):
         report_existed(id, Version)
     else:
-        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
     # Updating
