7130eb1460c7b872098c0bcb6d0f2dbd867274ce
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-01-27 09:36:57 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -2248,6 +2248,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+    # Add win32ss.Supermium to Update List
+    id = "win32ss.Supermium"
+    JSON = requests.get("https://api.github.com/repos/win32ss/supermium/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = re.search(r'[^\d.]', '', requests.get("https://api.github.com/repos/win32ss/supermium/releases/latest", verify=False, headers=Headers[1]).json()["name"])
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe")]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
