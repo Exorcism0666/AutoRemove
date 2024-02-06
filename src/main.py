c23a9a0f8dcf921669db8a20e9e08047cf34ba1d
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-02-06 22:40:29 +0000

    Update

--- src/main.py
+++ src/main.py
@@ -2483,6 +2483,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add Google.Brotli to Update List
+    id = "Google.Brotli"
+    JSON = requests.get("https://api.github.com/repos/google/brotli/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/google/brotli/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("zip" in each["browser_download_url"]) and ("static" in each["browser_download_url"]) and not("dynamic" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
