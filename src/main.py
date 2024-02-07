3f2696d6dbdecbd4e0f342c3c49abad965ee27ff
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2024-02-07 08:07:06 +0000

    Update

--- src/main.py
+++ src/main.py
@@ -2496,6 +2496,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+   # Add developersu.ns-usbloader to Update List
+    id = "developersu.ns-usbloader"
+    JSON = requests.get("https://api.github.com/repos/developersu/ns-usbloader/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/developersu/ns-usbloader/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not("legacy" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
