8e5a28d5641334e16404f94ff89510a46358b768
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-02-22 18:31:46 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -2850,8 +2850,8 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
-# Add Kubernates.kwok to Update List
-    id = "Kubernates.kwok"
+# Add Kubernetes.kwok to Update List
+    id = "Kubernetes.kwok"
     JSON = requests.get("https://api.github.com/repos/kubernetes-sigs/kwok/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/kubernetes-sigs/kwok/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
     Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe") and not("kwokctl" in each["browser_download_url"])]
