bbe5c76f8c68f4cc54482e49082d5e0f7586dc58
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-02-23 19:06:21 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -3054,6 +3054,18 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+# Add FilenCloud.FilenSync to Update List
+    id = "FilenCloud.FilenSync"
+    Version = requests.get("https://api.github.com/repos/FilenCloudDienste/filen-desktop/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [f"https://cdn.filen.io/desktop/release/filen_x64.exe"]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
