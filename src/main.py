ea5cd75e01c625e4dd11e46c1db687f54d7e1c1d
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-02-26 10:47:58 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -3079,6 +3079,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+# Add JGraph.Draw_Pckgr to Update List
+    id = "JGraph.Draw_Pckgr"
+    JSON = requests.get("https://api.github.com/repos/jgraph/drawio-desktop/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/jgraph/drawio-desktop/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".exe") and not(("no" in each["browser_download_url"]) or ("blockmap" in each["browser_download_url"]))]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
