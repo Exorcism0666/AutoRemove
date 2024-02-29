66ddebde5fc206a5356b68c5f473eeb444e01852
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-02-29 16:39:51 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3157,6 +3157,45 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
     del JSON, Urls, Version, id
 
+# Add GodotEngine.GodotEngine to Update List
+    id = "GodotEngine.GodotEngine"
+    JSON = requests.get("https://api.github.com/repos/godotengine/godot/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = re.search(r'\d+(\.\d+)+', requests.get("https://api.github.com/repos/godotengine/godot/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]).group()
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".zip") and not(("linux" in each["browser_download_url"]) or ("macos" in each["browser_download_url"]) or ("mono" in each["browser_download_url"]) or ("web" in each["browser_download_url"]))]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), Version, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+# Add prefix-dev.pixi to Update List
+    id = "prefix-dev.pixi"
+    JSON = requests.get("https://api.github.com/repos/prefix-dev/pixi/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/prefix-dev/pixi/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith(".msi")]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
+# Add ONLYOFFICE.DesktopEditors to Update List
+    id = "ONLYOFFICE.DesktopEditors"
+    JSON = requests.get("https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith((".msi", ".exe")) ("xp" in each["browser_download_url"])]
+    if not version_verify(str_pop(Version, 0), id):
+         report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Komac, id, list_to_str(Urls), str_pop(Version, 0), GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
     # Updating
     if not debug:
         for each in Commands:
