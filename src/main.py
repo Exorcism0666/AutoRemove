fb89bbf83b45971498c5959254de9852a83b3dbb
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-02-29 20:37:46 +0100

    FIx

--- src/main.py
+++ src/main.py
@@ -3187,7 +3187,7 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
     id = "ONLYOFFICE.DesktopEditors"
     JSON = requests.get("https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
     Version = requests.get("https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
-    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith((".msi", ".exe")) ("xp" in each["browser_download_url"])]
+    Urls = [each["browser_download_url"] for each in JSON if each["browser_download_url"].endswith((".msi", ".exe")) and not("xp" in each["browser_download_url"])]
     if not version_verify(str_pop(Version, 0), id):
          report_existed(id, Version)
     elif do_list(id, Version, "verify"):
