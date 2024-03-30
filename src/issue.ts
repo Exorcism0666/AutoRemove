3ae8757a63bdc8d9c42d0a62bf7e9760b1432aad
CoolPlayLin <CoolPlayLin@qq.com>
2024-03-30 12:23:18 +0800

    feat: avoid throw errors :sparkles:

--- src/issue.ts
+++ src/issue.ts
@@ -24,52 +24,62 @@ const main = async () => {
     if (searchedContent.length == 0) break;
     datas.push(searchedContent);
   }
-  datas.forEach((data) => {
-    data.forEach(async (obj) => {
-      const labels = (
-        await api.rest.issues.listLabelsOnIssue({
-          repo: repo,
-          owner: owner,
-          issue_number: obj.number,
-        })
-      ).data.map((label) => label.name);
-      if (labels.includes("No-Recent-Activity")) {
-        await api.rest.issues.createComment({
-          owner: owner,
-          repo: repo,
-          issue_number: obj.number,
-          body: `Don't close this Pull Request please :)\nCC @CoolPlayLin`,
-        });
-        console.log(`Prevent #${obj.issue_number} from being stale`);
-      }
-      const comments = await api.rest.issues.listComments({
-        repo: repo,
-        owner: owner,
-        issue_number: obj.number,
-      });
-      comments.data.forEach(async (comment) => {
-        if (comment.user.login !== owner_login) {
-          return;
-        }
-        const { body } = comment;
-        if (body.match(/@[Cc]ool[Pp]lay[Ll]in[Bb]ot [Cc]lose/)) {
-          console.log(`Close #${obj.number}`);
-          await api.rest.issues.createComment({
-            owner: owner,
+  console.log(
+    `${datas.length} ${datas.length === 0 ? "Pull Request" : "Pull Requests"} ${datas.length === 0 ? "is" : "are"} still opened`
+  );
+  for (let j = 0; j++; j < datas.length) {
+    setTimeout(async () => {
+      try {
+        datas[j].forEach(async (obj) => {
+          const labels = (
+            await api.rest.issues.listLabelsOnIssue({
+              repo: repo,
+              owner: owner,
+              issue_number: obj.number,
+            })
+          ).data.map((label) => label.name);
+          if (labels.includes("No-Recent-Activity")) {
+            await api.rest.issues.createComment({
+              owner: owner,
+              repo: repo,
+              issue_number: obj.number,
+              body: `Don't close this Pull Request please :)\nCC @CoolPlayLin`,
+            });
+            console.log(`Prevent #${obj.issue_number} from being stale`);
+          }
+          const comments = await api.rest.issues.listComments({
             repo: repo,
+            owner: owner,
             issue_number: obj.number,
-            body: `I've received my owner's request to close this pr, I'll close it right now`,
           });
-          await api.rest.pulls.update({
-            owner: owner,
-            repo: repo,
-            pull_number: obj.number,
-            state: "closed",
+          comments.data.forEach(async (comment) => {
+            if (comment.user.login !== owner_login) {
+              return;
+            }
+            const { body } = comment;
+            if (body.match(/@[Cc]ool[Pp]lay[Ll]in[Bb]ot [Cc]lose/)) {
+              console.log(`Close #${obj.number}`);
+              await api.rest.issues.createComment({
+                owner: owner,
+                repo: repo,
+                issue_number: obj.number,
+                body: `The request from my owner require me to close this Pull Request\n\nThis Pull Request will always be close if the following comment still exist\n> CoolPlayLin \n>${body}`,
+              });
+              await api.rest.pulls.update({
+                owner: owner,
+                repo: repo,
+                pull_number: obj.number,
+                state: "closed",
+              });
+              console.log(`Closes #${obj.number}`);
+            }
           });
-        }
-      });
-    });
-  });
+        });
+      } catch (error) {
+        console.log(`Request failed`);
+      }
+    }, 500 * j);
+  }
 };
 
 main().catch((error) => {
